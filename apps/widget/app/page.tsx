cdc5841342d4d0c1f9cc358367d28f961a09290b
alexander <alexander_199@hotmail.se>
2025-09-05 08:43:08 +0200

    intial commit

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
+    const addUser = useMutation(api.users.add);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
